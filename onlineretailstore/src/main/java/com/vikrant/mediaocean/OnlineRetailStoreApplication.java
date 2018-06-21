eb72ddc3bc1fe57dcb36a683be4191035b549bc7
Vikrant Dheer <vikrant28may@gmail.com>
2018-06-21 20:03:59 +0530

    spring boot up

---
+++ onlineretailstore/src/main/java/com/vikrant/mediaocean/OnlineRetailStoreApplication.java
@@ -0,0 +1,11 @@
+package com.vikrant.mediaocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRetailStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineRetailStoreApplication.class, args);
+    }
+}
